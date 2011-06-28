4e0b5ad903a7a66c54bae348063ee946c17ea6a1
Adith Radityo <adith@zenstealth.com>
2011-06-28 02:52:08 -0400

    Updated Google Analytics

--- javascript/analytics.js
+++ javascript/analytics.js
@@ -1,10 +1,13 @@
-  var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-11888578-1']);
-_gaq.push(['_setDomainName', '.zenstealth.com']);
-_gaq.push(['_trackPageview']);
+<script type="text/javascript">
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24220862-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
