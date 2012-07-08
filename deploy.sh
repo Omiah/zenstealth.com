7d3e356967a1d2d2eff3c834cddc351b4493329a
Adith Radiyo <adith@zenstealth.com>
2012-07-08 00:14:07 -0400

    Safer local deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # Generate static files via Jekyll
 jekyll --no-auto
 # Transfers modified files to NFSN
-rsync -avz --delete _site/ nfsn-zen:/home/public
+rsync -avzh --delete _site/ nfsn-zen:/home/public
